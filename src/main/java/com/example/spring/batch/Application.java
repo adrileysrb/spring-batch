ac3ab8211f42c0c0ffed23b34c90581a52cf02ed
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2025-03-30 07:57:32 -0300

    chore: downgrade spring version and build first job

--- src/main/java/com/example/spring/batch/Application.java
+++ src/main/java/com/example/spring/batch/Application.java
@@ -2,8 +2,12 @@ package com.example.spring.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan("com.example.spring.batch.config")
 public class Application {
 
 	public static void main(String[] args) {
